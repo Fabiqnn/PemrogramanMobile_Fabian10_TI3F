c85214868240a1389f02d42b71f67b6fc4f6f0ab
Fabiqnn <arema20056@gmail.com>
2025-11-19 13:27:57 +0700

    W13: soal 1

---
+++ codelab13/store_data_fabian/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON Demo - Fabian',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('JSON'),),
+      body: Container(),
+    );
+  }
+}
